c2e7d1d4660e5524deab0c6e7922d9578a63e8ca
jabaraster <ah@jabara.info>
2018-12-05 03:12:27 +0900

    save.

--- src/Jabara/Util/Xlsx.hs
+++ src/Jabara/Util/Xlsx.hs
@@ -1,9 +1,15 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Jabara.Util.Xlsx (
   RowIndex
   , ColumnIndex
   , SheetName
+  , CellLocation(..)
+
   , readBook
+  , readBookUnsafe
+
+  , parseCellLocationText
 
   , cellBoolValue
   , cellBoolValueFromSheet
@@ -32,25 +38,33 @@ module Jabara.Util.Xlsx (
 ) where
 
 import           Codec.Xlsx
+import           Control.Exception
 import           Control.Lens
 import           Data.ByteString.Lazy (readFile)
 import           Data.Maybe
 import           Data.Text
 import           Data.Time.Calendar
 import           Prelude              hiding (readFile)
+import qualified Text.Parsec as TP
 
-readBook :: FilePath -> IO Xlsx
-readBook path = toXlsx <$> readFile path
+readBook :: FilePath -> IO (Maybe Xlsx)
+readBook path = (Just . toXlsx <$> readFile path) `catch` \(_::SomeException) -> pure Nothing
+
+readBookUnsafe :: FilePath -> IO Xlsx
+readBookUnsafe path = fromJust <$> readBook path
 
 type SheetName = Text
 type RowIndex = Int
 type ColumnIndex = Int
 
-cellBoolValueFromSheet :: Worksheet -> (RowIndex, ColumnIndex) -> Bool
-cellBoolValueFromSheet sheet cell = cellBoolValue $ sheet ^? ixCell cell . cellValue . _Just
+newtype CellLocation = CellLocation (RowIndex, ColumnIndex)
+  deriving (Show, Eq, Read)
+
+cellBoolValueFromSheet :: Worksheet -> CellLocation -> Bool
+cellBoolValueFromSheet sheet (CellLocation (r,c)) = cellBoolValue $ sheet ^? ixCell (r+1,c+1) . cellValue . _Just
 
 cellBoolValueFromSheetM :: Worksheet -> (RowIndex, ColumnIndex) -> Maybe Bool
-cellBoolValueFromSheetM sheet cell = cellBoolValueM $ sheet ^? ixCell cell . cellValue . _Just
+cellBoolValueFromSheetM sheet (r,c) = cellBoolValueM $ sheet ^? ixCell (r+1,c+1) . cellValue . _Just
 
 cellBoolValue :: Maybe CellValue -> Bool
 cellBoolValue Nothing = error "cell not found"
@@ -63,10 +77,10 @@ cellBoolValueM (Just (CellBool b)) = Just b
 cellBoolValueM (Just _)            = Nothing
 
 cellDoubleValueFromSheet :: Worksheet -> (RowIndex, ColumnIndex) -> Double
-cellDoubleValueFromSheet sheet cell = cellDoubleValue $ sheet ^? ixCell cell . cellValue . _Just
+cellDoubleValueFromSheet sheet (r,c) = cellDoubleValue $ sheet ^? ixCell (r+1,c+1) . cellValue . _Just
 
 cellDoubleValueFromSheetM :: Worksheet -> (RowIndex, ColumnIndex) -> Maybe Double
-cellDoubleValueFromSheetM sheet cell = cellDoubleValueM $ sheet ^? ixCell cell . cellValue . _Just
+cellDoubleValueFromSheetM sheet (r,c) = cellDoubleValueM $ sheet ^? ixCell (r+1,c+1) . cellValue . _Just
 
 cellDoubleValue :: Maybe CellValue -> Double
 cellDoubleValue Nothing = 0
@@ -79,10 +93,10 @@ cellDoubleValueM (Just (CellDouble d)) = Just d
 cellDoubleValueM (Just _)              = Nothing
 
 cellIntegralValueFromSheet :: (Integral a) => Worksheet -> (RowIndex, ColumnIndex) -> a
-cellIntegralValueFromSheet sheet cell = cellIntegralValue $ sheet ^? ixCell cell . cellValue . _Just
+cellIntegralValueFromSheet sheet (r,c) = cellIntegralValue $ sheet ^? ixCell (r+1,c+1) . cellValue . _Just
 
 cellIntegralValueFromSheetM :: (Integral a) => Worksheet -> (RowIndex, ColumnIndex) -> Maybe a
-cellIntegralValueFromSheetM sheet cell = cellIntegralValueM $ sheet ^? ixCell cell . cellValue . _Just
+cellIntegralValueFromSheetM sheet (r,c) = cellIntegralValueM $ sheet ^? ixCell (r+1,c+1) . cellValue . _Just
 
 cellIntegralValue :: (Integral a) => Maybe CellValue -> a
 cellIntegralValue Nothing = 0
@@ -94,11 +108,11 @@ cellIntegralValueM Nothing               = Nothing
 cellIntegralValueM (Just (CellDouble d)) = Just $ floor d
 cellIntegralValueM (Just c)              = Nothing
 
-cellStringValueFromSheet :: Worksheet -> (RowIndex, ColumnIndex) -> Text
-cellStringValueFromSheet sheet cell = cellStringValue $ sheet ^? ixCell cell . cellValue . _Just
+cellStringValueFromSheet :: Worksheet -> CellLocation -> Text
+cellStringValueFromSheet sheet (CellLocation (r,c)) = cellStringValue $ sheet ^? ixCell (r+1,c+1) . cellValue . _Just
 
 cellStringValueFromSheetM :: Worksheet -> (RowIndex, ColumnIndex) -> Maybe Text
-cellStringValueFromSheetM sheet cell = cellStringValueM $ sheet ^? ixCell cell . cellValue . _Just
+cellStringValueFromSheetM sheet (r,c) = cellStringValueM $ sheet ^? ixCell (r+1,c+1) . cellValue . _Just
 
 cellStringValue :: Maybe CellValue -> Text
 cellStringValue Nothing = ""
@@ -111,10 +125,10 @@ cellStringValueM (Just (CellText t)) = Just t
 cellStringValueM (Just _)            = Nothing
 
 cellDayValueFromSheet :: Worksheet -> (RowIndex, ColumnIndex) -> Day
-cellDayValueFromSheet sheet cell = cellDayValue $ sheet ^? ixCell cell . cellValue . _Just
+cellDayValueFromSheet sheet (r,c) = cellDayValue $ sheet ^? ixCell (r+1,c+1) . cellValue . _Just
 
 cellDayValueFromSheetM :: Worksheet -> (RowIndex, ColumnIndex) -> Maybe Day
-cellDayValueFromSheetM sheet cell = cellDayValueM $ sheet ^? ixCell cell . cellValue . _Just
+cellDayValueFromSheetM sheet (r,c) = cellDayValueM $ sheet ^? ixCell (r+1,c+1) . cellValue . _Just
 
 cellDayValue :: Maybe CellValue -> Day
 cellDayValue Nothing = error "cell not found"
@@ -125,3 +139,22 @@ cellDayValueM :: Maybe CellValue -> Maybe Day
 cellDayValueM Nothing = Nothing
 cellDayValueM mCell = let dayValue = cellIntegralValue mCell
                       in  Just $ addDays (dayValue - 2) $ fromGregorian 1900 1 1
+
+parseCellLocationText :: Text -> Maybe CellLocation
+parseCellLocationText s = case parseCore s of
+  Left _ -> Nothing
+  Right cell -> Just cell
+  where
+    parseCore :: Text -> Either TP.ParseError CellLocation
+    parseCore = TP.parse coreParser "" . unpack
+
+    coreParser :: TP.ParsecT String u Identity CellLocation
+    coreParser = do
+      colString <- TP.many1 TP.upper
+      row <- rowParser
+      return $ CellLocation (row, 0)
+
+    rowParser :: TP.ParsecT String u Identity ColumnIndex
+    rowParser = do
+      rowString <- TP.many1 TP.digit
+      return $ flip (-) 1 $ read rowString
